35729802f51d911d62348b1c7002810f014c237b
xiaochuanfang <xiaochuan1991@hotmail.com>
2019-03-27 23:46:56 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/parseinstagram/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.codepath.parseinstagram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG="LoginActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        etUsername=findViewById(R.id.etUsername);
+        etPassword=findViewById(R.id.etPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=etUsername.getText().toString();
+                String password=etPassword.getText().toString();
+                login(username,password);
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e!=null){
+                    Log.e(TAG,"Issue with login");
+                    e.printStackTrace();
+                    return;
+                }
+
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Log.d(TAG,"Navigating to Main activity");
+        Intent i=new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
